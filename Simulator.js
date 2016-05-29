7b1efd7188f8e77615dfb8bf83347656790e5eab
ayushk92 <ayushkhatri2006@gmail.com>
2016-05-29 11:13:25 +0530

    Version 1 simulator

---
+++ Simulator.js
@@ -0,0 +1,168 @@
+var fs = require("fs");	
+var querystring = require("querystring");
+var https = require("https");
+var host = 'api.havenondemand.com';
+
+var current_JOBID = "";
+var jobPath = "https://api.havenondemand.com/1/job/result/";
+var api_key = "apikey=53656730-a649-4644-9b95-4d347eb831be";
+var outputValue = ""
+var index = 0;
+
+
+
+var simulationExecution = {
+	//simulation_results : [],
+	readJSONFile : function(){
+		this.input  = fs.readFileSync( "simulation.json", 'utf8');  
+		//console.log("Input : " + JSON.stringify(this.input));		
+	},
+	getJobResult : function(jobId,simulationCallback){
+		console.log("Checking job ID");
+		var endpoint = jobPath + jobId + '?' + api_key ;
+		console.log(endpoint);
+		var options = {
+		host: host,
+		path: endpoint,
+		method: 'GET',
+		};
+
+		var req = https.request(options, function(res) {
+		res.setEncoding('utf-8');
+
+		var responseString = '';
+
+		res.on('data', function(data) {
+		  console.log(data + " end.")
+		  responseString += data;
+		});
+
+		res.on('end', function(data) {
+		  //console.log(responseString);
+		  var responseObject = JSON.parse(responseString);
+		  //console.log(responseObject);		  
+		  console.log("Callback is executing.");
+		  simulationCallback(responseObject);
+		  //	console.log(responseObject);
+		});
+		});
+		req.write(api_key);
+		req.end();
+	},
+	executeSimulation : function() {	
+		//var simulationSteps = JSON.parse(this.input);	
+		console.log("Current index: " + this.index);
+		if(this.index < this.simulationSteps.length){
+		 	var simulation = this.simulationSteps[this.index];
+		 	simulation.request = this.processRequestParams(simulation.request);	
+		 	current_JOBID = undefined;	 		 	
+		 	this.performRequest(simulation.api_url,'GET',simulation.request,simulationCallback);
+		 	this.isStepExecuted = true;
+		 	this.index++;
+		 	//this.post(simulation.simulation_id,simulation.api_url,simulation.request, "POST");
+		}		 		 
+	},
+	simulationDriver : function(){
+		this.simulation_results = new Array();
+		this.simulationSteps = JSON.parse(this.input);
+		this.index = 0;
+		this.executeSimulation();
+	},
+	processRequestParams : function(requestParams){
+		if(outputValue != ""){
+			Object.keys(requestParams).forEach(function(item,index){
+				requestParams[item] = outputValue;
+			});
+		}
+		return requestParams;
+	},
+	performRequest : function(endpoint, method, data, simulationCallback) {
+		var dataString = JSON.stringify(data);
+		var headers = {};
+
+		if (method == 'GET') {
+		endpoint += '?' + api_key + "&"+ querystring.stringify(data);
+		}
+		else {
+		headers = {
+		  'Content-Type': 'application/json',
+		  'Content-Length': dataString.length
+
+		};
+		}
+		console.log("Endpoint: " + endpoint);
+		var options = {
+		host: host,
+		path: endpoint,
+		method: method,
+		headers: headers
+		};
+
+		var req = https.request(options, function(res) {
+		res.setEncoding('utf-8');
+
+		var responseString = '';
+
+		res.on('data', function(data) {
+		  responseString += data;
+		});
+
+		res.on('end', function(data) {
+		  console.log(responseString);
+		  var responseObject = JSON.parse(responseString);
+		  console.log(responseObject.jobID);
+		  //current_JOBID = responseObject.jobID;
+		  simulationCallback(responseObject);
+		  
+		});
+	});
+		console.log("Data string: "+dataString);
+		req.write("");
+		req.end();
+	}
+
+}
+function isArray(what) {
+    return Object.prototype.toString.call(what) === '[object Array]';
+}
+function getJSONKeyValue(myJSON,outputKey){
+	var keys = outputKey.split(".");
+	outputValue = "";
+	keys.forEach(function(item,index){
+		if(myJSON.hasOwnProperty(item)){
+			if(isArray(myJSON[item])){
+				myJSON = myJSON[item][0];
+			}
+			else{
+				myJSON = myJSON[item];
+			}	
+		}		
+	});
+	return myJSON;
+	
+}
+
+function checkJobStatusCallback(jobID){
+	simulationExecution.getJobResult(jobID,simulationCallback);
+}
+function simulationCallback(responseObject){
+
+	if(responseObject.jobID != undefined && responseObject.status == undefined){
+		console.log("JobId: " + responseObject.jobID)
+		checkJobStatusCallback(responseObject.jobID);
+	}
+	else{
+		var index = simulationExecution.index - 1;
+		var outputKey = simulationExecution.simulationSteps[index].response
+		outputValue = JSON.stringify(getJSONKeyValue(responseObject,outputKey));
+		console.log("Output value :" + outputValue);
+		simulationExecution.executeSimulation();
+	}	
+}
+
+
+
+simulationExecution.readJSONFile();
+simulationExecution.simulationDriver();
+
+//console.log("Input : " + JSON.parse(simulationExecution.input)[0]);
